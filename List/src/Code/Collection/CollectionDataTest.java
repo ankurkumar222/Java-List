f5eb33fddaabb2157e179c7af68d01128c4ceaac
ankurkumar222 <ankur.cse.bvcoe@gmail.com>
2015-12-19 23:25:13 -0800

    commit

---
+++ List/src/Code/Collection/CollectionDataTest.java
@@ -0,0 +1,25 @@
+package Code.Collection;
+//: containers/CollectionDataTest.java
+import java.util.*;
+
+import Util.Generator;
+import Util.*;
+
+class Government implements Generator<String> {
+	String[] foundation = ("strange women lying in ponds distributing swords is no basis for a system of government").split(" ");
+	private int index;
+
+	public String next() {
+		return foundation[index++];
+	}
+}
+
+public class CollectionDataTest {
+	public static void main(String[] args)
+	{
+		Set<String> set = new LinkedHashSet<String>(new CollectionData<String>(	new Government(), 15));
+
+		set.addAll(CollectionData.list(new Government(), 15));
+		System.out.println(set);
+	}
+}
---
+++ List/src/Util/Generator.java
@@ -0,0 +1,6 @@
+// A generic interface.
+package Util;
+
+public interface Generator<T> {
+	T next();
+} 
